3a7ca5ed82917b7538b8a9047dc768bbf7118ac7
danghanh99 <ldhanh1999@gmail.com>
2021-01-29 11:31:45 +0700

    NVL 18: add SQL unique constraint for phone_number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_074852) do
+ActiveRecord::Schema.define(version: 2021_01_29_042253) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_01_28_074852) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["phone_number"], name: "index_customers_on_phone_number", unique: true
   end
 
   create_table "exports", force: :cascade do |t|
