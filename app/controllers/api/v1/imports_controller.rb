9e455415fb60599ad9d031578effea5fbecb69a2
quanghuy30101999 <huytran.301099@gmail.com>
2021-02-05 09:14:20 +0700

    NVL-48-Delete an import

--- app/controllers/api/v1/imports_controller.rb
+++ app/controllers/api/v1/imports_controller.rb
@@ -1,10 +1,19 @@
 class Api::V1::ImportsController < ApplicationController
+  before_action :find_import, only: %i[destroy]
   def create
     import = Import.create!(import_params)
     render_resource import, :created, ImportSerializer
   end
 
+  def destroy
+    @import.destroy
+  end
+
   private
+
+  def find_import
+    @import = Import.find(params[:id])
+  end
 
   def import_params
     params.permit(:inventory_id, :supplier_id, :product_id, :retail_price, :quantity, :description, :imported_date, :user_id)
