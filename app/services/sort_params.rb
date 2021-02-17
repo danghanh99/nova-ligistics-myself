0fb63fed63f5c7e350bbae48c16ec0a4556df69a
quanghuy30101999 <huytran.301099@gmail.com>
2021-02-17 14:40:10 +0700

    sort product and supplier with export

--- app/services/sort_params.rb
+++ app/services/sort_params.rb
@@ -3,12 +3,20 @@ class SortParams
 
   SORT_ORDERS = %w[asc desc].freeze
 
+  ACTION_RULES = {
+    'product_name' => 'products.name',
+    'supplier_name' => 'suppliers.name',
+    'inventory_name' => 'inventories.name',
+    'sell_price' => 'exports.sell_price',
+    'quantity' => 'exports.quantity',
+    'exported_date' => 'exports.exported_date'
+  }.freeze
+
   def initialize(sort_params, class_name = nil)
+    @sort_query = sort_params.tr(':', ' ')
     if class_name.nil?
-      @sort_type = sort_params
       validate_sort_by_a_filter
     else
-      @sort_query = sort_params.tr(':', ' ')
       @class_name = class_name
       validate_sort_by_many_filters
     end
@@ -22,6 +30,8 @@ class SortParams
   end
 
   def validate_sort_by_a_filter
-    raise(ExceptionHandler::BadRequest, 'Invalid sort type') unless SORT_ORDERS.include?(@sort_type.downcase)
+    column, sort_type = @sort_query.split(' ')
+    @sort_query.sub!(column, ACTION_RULES[column]) if ACTION_RULES.include?(column)
+    raise(ExceptionHandler::BadRequest, 'Invalid sort type') unless SORT_ORDERS.include?(sort_type.downcase)
   end
 end
