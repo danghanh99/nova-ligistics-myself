bb36a104f6fcedfeae607a9e15839e1ae4484c4b
danghanh99 <ldhanh1999@gmail.com>
2021-02-25 19:34:58 +0700

    NVL: add inventories, suppliers, imports to ProductSerializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,4 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :created_at, :total_quantity, :total_available_quantity
   has_many :imports
+  attributes :id, :name, :description, :created_at, :total_quantity, :total_available_quantity, :inventories, :suppliers, :imports
+  class ImportSerializer < ActiveModel::Serializer
+    attributes :id, :retail_price, :quantity, :available_quantity, :description, :imported_date, :created_at
+  end
 end
